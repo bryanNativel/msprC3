a1f807a893b2f39362c494e6456daaea02e4d187
Bryan Nativel <bryan.nativel@hotmail.fr>
2021-03-08 09:30:43 +0100

    Error Qrscanner don't open

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private qrScanner: QRScanner) { }
+
+  ngOnInit() {}
+
+  public scanQrCode(): void{
+    console.log("entre dans la methode");
+    this.qrScanner.prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          console.log("if");
+          // camera permission was granted
+          // start scanning
+          const scanSub = this.qrScanner.scan().subscribe((text: string) => {
+            console.log('Scanned something', text);
+
+            this.qrScanner.hide(); // hide camera preview
+            scanSub.unsubscribe(); // stop scanning
+          });
+
+        } else if (status.denied) {
+          console.log("esseIf");
+          // camera permission was permanently denied
+          // you must use QRScanner.openSettings() method to guide the user to the settings page
+          // then they can grant the permission from there
+        } else {
+          // permission was denied, but not permanently. You can ask for permission again at a later time.
+          console.log("else");
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+  }
+
+}
