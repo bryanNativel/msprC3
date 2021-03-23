64d1a282024175d94d5e86231f8bc2d66b146db3
Bryan Nativel <bryan.nativel@hotmail.fr>
2021-03-23 19:58:53 +0100

    not important commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { DetailComponent } from './detail/detail.component';
 import {LoginComponent} from './login/login.component';
-import {AuthGuardService} from './_helpers/auth-guard.service';
 import {RegisterComponent} from './register/register.component';
+import {AuthGuardService} from './_helpers/auth-guard.service';
+import {DetailComponent} from './detail/detail.component';
 
 const routes: Routes = [
   {
