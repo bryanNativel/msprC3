15429aa41c2bd8c3417d12b49f65ba0523e153dd
Bryan Nativel <bryan.nativel@hotmail.fr>
2021-03-29 22:35:43 +0200

    wip

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
-
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   exports: [
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule {}
