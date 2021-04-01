d4661d7d49b70c2d8d282098c33c71a0973ae210
aminebirani <a.birani.elhbak@gmail.com>
2021-04-01 11:59:13 +0200

    update(historic): Update Historic component to use  new HistoricService

--- src/app/historic/historic.component.ts
+++ src/app/historic/historic.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {QrCodeRequestService} from '../service/qrCode-request.service';
 import {QrCode} from '../interface/qr-code';
-import {Observable, of} from 'rxjs';
+import {HistoricService} from "../service/database/historic.service";
+import {Observable} from "rxjs";
+import {IHistoric} from "../interface/ihistoric";
+import {concatMap, filter, flatMap, map} from "rxjs/operators";
+import {AuthService} from "../service/auth.service";
 
 @Component({
   selector: 'app-historic',
@@ -10,9 +14,14 @@ import {Observable, of} from 'rxjs';
 })
 export class HistoricComponent implements OnInit {
 
-  constructor(private serviceRequest: QrCodeRequestService) {}
-  public historicQrCode: QrCode[];
+  constructor(private serviceRequest: QrCodeRequestService, private historicDao: HistoricService, private authService: AuthService) {}
+  public historicQrCode$: Observable<IHistoric[]>;
+  public userId: number
+
   ngOnInit() {
+    this.userId = 1 //this.authService.getUserId()
+    this.historicQrCode$ = this.userId ? this.historicDao.getAllForUser(this.userId) : this.historicDao.getAll()
+  }
 
     const qrcodeObject =  this.serviceRequest.getQrCodeHistoric('1').subscribe( {
         next: value => this.historicQrCode = value,
